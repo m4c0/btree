10274e691bd13be8f62699b377e91afd0cdfc835
Eduardo Costa <m4c0@users.noreply.github.com>
2023-07-08 20:24:47 +0100

    build: using the same idea from rtree

--- build.cpp
+++ build.cpp
@@ -3,7 +3,11 @@
 int main(int argc, char **argv) {
   using namespace ecow;
 
+  auto poc = unit::create<tool>("poc");
+  poc->add_unit<mod>("poc");
+
   auto all = unit::create<seq>("all");
-  all->add_unit<>("poc");
+  all->add_ref(poc);
+  all->add_unit<sys>(poc->executable().string());
   return run_main(all, argc, argv);
 }
